485e15cd40067f432176a73200371c41120344d4
kickcik <4778009@gmail.com>
2025-09-05 19:27:02 +0900

    formatting, linter, mypy tester

---
+++ test.sh
@@ -0,0 +1,25 @@
+set -eo pipefail
+
+COLOR_GREEN=`tput setaf 2;`
+COLOR_NC=`tput sgr0;` # No Color
+
+echo "Starting black"
+poetry run black .
+echo "OK"
+
+echo "Starting ruff"
+poetry run ruff check --select I --fix
+poetry run ruff check --fix
+echo "OK"
+
+echo "Starting dmypy"
+if poetry run dmypy status > /dev/null 2>&1; then
+    poetry run dmypy run -- .
+else
+    echo "Succeeding daemon's body..."
+    echo "mypy is coming..."
+    poetry run mypy .
+fi
+echo "OK"
+
+echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
