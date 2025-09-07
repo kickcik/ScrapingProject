64cebe79a39e5f7d3237a12f323205e0163b3822
kickcik <4778009@gmail.com>
2025-09-07 16:35:10 +0900

    alter quotes table's 'content' column type

--- app/models/quotes.py
+++ app/models/quotes.py
@@ -4,7 +4,7 @@ from app.models.base import BaseModel
 
 
 class Quote(BaseModel, Model):
-    content = fields.CharField(required=True, max_length=255)
+    content = fields.TextField(required=True)
     author = fields.CharField(required=True, max_length=255)
 
     class Meta:
