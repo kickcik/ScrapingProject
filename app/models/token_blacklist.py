cde790672c4e569071b8731ec8d241370cd95998
kickcik <4778009@gmail.com>
2025-09-08 15:45:33 +0900

    edit column types, add index

--- app/models/token_blacklist.py
+++ app/models/token_blacklist.py
@@ -5,11 +5,12 @@ from app.models.users import User
 
 
 class TokenBlacklist(BaseModel, Model):
-    token = fields.CharField(max_length=255)
-    expired_at = fields.DatetimeField(null=True)
+    token = fields.CharField(max_length=512, unique=True)
+    expired_at = fields.DatetimeField(null=False)
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
         "models.User", related_name="token_blacklist", on_delete=fields.CASCADE
     )
 
     class Meta:
         table = "token_blacklist"
+        indexes = [("expired_at",)]
