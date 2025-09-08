41e7973ff441e9014b3dee9103891dc8c98de934
kickcik <4778009@gmail.com>
2025-09-08 13:50:13 +0900

    delete verify_password

--- app/models/users.py
+++ app/models/users.py
@@ -1,14 +1,11 @@
 from tortoise import Model, fields
 
 from app.models.base import BaseModel
-from passlib.hash import bcrypt
+
 
 class User(BaseModel, Model):
     username = fields.CharField(unique=True, max_length=255)
     password_hash = fields.CharField(max_length=255)
-
-    def verify_password(self, password: str) -> bool:
-        return bcrypt.verify(password, self.password_hash)
 
     class Meta:
         table = "users"
