2b0dc346bf26789cf668ba66ce1df41b1024db70
kickcik <4778009@gmail.com>
2025-09-08 16:42:36 +0900

    delete created_at filed

--- app/models/diaries.py
+++ app/models/diaries.py
@@ -7,7 +7,6 @@ from app.models.users import User
 class Diary(BaseModel, Model):
     title = fields.CharField(required=True, max_length=255)
     content = fields.TextField(required=True, max_length=255)
-    created_at = fields.DatetimeField(required=True, auto_now=True)
 
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
         "models.User", related_name="diaries", on_delete=fields.CASCADE
