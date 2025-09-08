76ec5d5650b2174ac031aad9693f6ee9ad65b8d8
kickcik <4778009@gmail.com>
2025-09-08 14:12:06 +0900

    add pytest

--- test.sh
+++ test.sh
@@ -22,4 +22,9 @@ else
 fi
 echo "OK"
 
+echo "Starting pytest with coverage"
+poetry run coverage run -m pytest
+poetry run coverage report -m
+poetry run coverage html
+
 echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
