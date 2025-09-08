58b217f8f669671123904ee1ddba78d6a5c22506
kickcik <4778009@gmail.com>
2025-09-08 16:43:12 +0900

    include diary router

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.configs.database import initialize_tortoise
+from app.routers.diary_router import diary_router
 from app.routers.user_router import user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(diary_router)
 
 initialize_tortoise(app=app)
 
