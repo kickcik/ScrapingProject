b2b434653df81ce14e0b0fa367fc16ddc0f59221
kickcik <4778009@gmail.com>
2025-09-12 19:29:53 +0900

    include bookmark router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.configs.database import initialize_tortoise
+from app.routers.bookmark_router import bookmark_router
 from app.routers.diary_router import diary_router
 from app.routers.quote_router import quote_router
 from app.routers.user_router import user_router
@@ -11,6 +12,7 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(diary_router)
 app.include_router(quote_router)
+app.include_router(bookmark_router)
 
 initialize_tortoise(app=app)
 
